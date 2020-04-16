77017a4c3fdaacdb95ccc55e9c3d844a5333eada
gnanesh007.mk@gmail.com <gnanesh007.mk@gmail.com>
2020-04-16 21:33:03 +0530

    initial commit

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,39 @@
+# todos_controller.rb
+class TodosController < ApplicationController
+  def index
+    #render plain: Todo.order(:due_date).map { |todo| todo.to_pleasant_string }.join("\n")
+    render "index"
+  end
+
+  def show
+    id = params[:id]
+    todo = Todo.find(id)
+    render plain: todo.to_pleasant_string
+  end
+
+  def create
+    todo_text = params[:todo_text]
+    due_date = DateTime.parse(params[:due_date])
+    new_todo = Todo.create!(todo_text: todo_text,
+                            due_date: due_date,
+                            completed: false)
+
+    redirect_to todos_path
+  end
+
+  def update
+    id = params[:id]
+    completed = params[:completed]
+    todo = Todo.find(id)
+    todo.completed = completed
+    todo.save!
+    redirect_to todos_path
+  end
+
+  def destroy
+    id = params[:id]
+    todo = Todo.find(id)
+    todo.destroy
+    redirect_to todos_path
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  get "/" => "home#index"
+
+  resources :todos
+  resources :users
+  post "users/login", to: "users#login"
+end
