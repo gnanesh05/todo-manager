2299997b1469aefb367c315faa5ea3b6a19f6e0b
gnanesh007.mk@gmail.com <gnanesh007.mk@gmail.com>
2020-04-18 20:29:26 +0530

    user can sign-out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,6 +10,13 @@ class SessionsController < ApplicationController
       session[:current_user_id] = user.id
       redirect_to todos_path
     else
+      render plain: "incorrect password/mail"
     end
+  end
+
+  def destroy
+    session[:current_user_id] = nil
+    @current_user = nil
+    redirect_to "/"
   end
 end
