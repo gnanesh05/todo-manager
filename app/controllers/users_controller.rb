1380d1a46c5d3c829f0576f00780960ec0be478b
gnanesh007.mk@gmail.com <gnanesh007.mk@gmail.com>
2020-04-19 14:03:58 +0530

    used validations to avoid invalid user sign-up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,15 @@ class UsersController < ApplicationController
   end
 
   def create
-    User.create!(first_name: params[:first_name],
-                 last_name: params[:last_name],
-                 email: params[:email],
-                 password: params[:password])
-    redirect_to "/"
+    new_user = User.new(first_name: params[:first_name],
+                        last_name: params[:last_name],
+                        email: params[:email],
+                        password: params[:password])
+    if new_user.save
+      redirect_to "/"
+    else
+      flash[:error] = new_user.errors.full_messages.join(", ")
+      redirect_to users_path
+    end
   end
 end
